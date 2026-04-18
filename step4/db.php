f05cdd60bcf5675888289256f2fb62956d9b8111
soumi <asma06zm@gmail.com>
2026-04-18 11:11:32 +0100

    Update db.php

--- step4/db.php
+++ step4/db.php
@@ -1,1 +1,14 @@
+<?php
+$host   = 'localhost';
+$dbname = 'gpa_db';
+$user   = 'root';
+$pass   = '';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode(['success' => false, 'message' => 'DB connection failed.']);
+    exit;
+}
+?>
